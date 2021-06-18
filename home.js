b31d4a0f6c8c6ae02092c8ff1236a154bcaebe4d
Rodrigo Blanca <rodrigoblanca96@gmail.com>
2021-06-18 12:21:08 -0300

    Desafío de la clase 02

--- home.js
+++ home.js
@@ -1,7 +1,38 @@
-const nombre = prompt('¿Cuál es tu nombre?');
+//Clase 01
+
+/* const nombre = prompt('¿Cuál es tu nombre?');
 
 const apellido = prompt('¿Cuál es tu apellido)');
 
 const saludo = "Hola " + nombre + " " + apellido + "!!";
 
-alert(saludo);
+alert(saludo); */
+
+
+//Clase 02
+
+const edad = prompt('¿Cuál es tu edad?');
+const acompañado = prompt('¿Viene acompañado?');
+
+
+//Primer condición si la persona es mayor a 18.
+if(parseInt(edad) >= 18) {
+
+    //En caso de cumplirse, la segunda condición, si viene acompañado.
+    if(acompañado == "Si") {
+        alert('Puede pasar y tiene un descuento');
+    } else {
+        alert('Puede pasar pero no tiene descuento');
+    }
+} else if ((parseInt(edad) < 18) && (acompañado == "Si")) {
+    //Si la persona es menor y está acompañada. Y si ese acompañante es mayor, puede pasar, si es menor no.
+    const acompañante = prompt('¿Qué edad tiene el acompañante?');
+    if (parseInt(acompañante) >= 18) {
+        alert('Puede pasar con el acompañante adulto');
+    } else {
+        alert('No puede pasar si el acompañante es menor de edad también');
+    }
+} else {
+    //En caso de que no se cumpla alguna de las anteriores, no puede pasar por ser menor o por ser menor y no estar acompañado por un adulto.
+    alert('No puede pasar');
+}
